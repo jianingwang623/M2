7cb244f569836a8b9bfb7c13ceca4ddff5d44c7f
ebills72 <ebills72@gmail.com>
2019-01-21 16:05:01 -0500

    Ethan Bills edited Person1.

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,9 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  String firstHalf = input.substring(0,2);
+	  String secondHalf = input.substring(2);
+	  return secondHalf + firstHalf;
 	}
 	
 	/**
